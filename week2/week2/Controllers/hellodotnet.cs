1ad9a06af97b6cf62f4c59ee987a24c42dd7161d
Fayis KV <mohammedfayis135@gmail.com>
2025-09-03 15:32:18 +0530

    Add project files.

---
+++ week2/week2/Controllers/hellodotnet.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace week2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class hellodotnet : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("hello, dotnet core web API.");
+        }
+    }
+}
