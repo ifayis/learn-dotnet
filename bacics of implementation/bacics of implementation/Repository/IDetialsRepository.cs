5aeb27e27b9a70ca844a384e1947d310b18b0ad4
Fayis KV <mohammedfayis135@gmail.com>
2026-02-19 11:28:55 +0530

    Added a new repository

---
+++ bacics of implementation/bacics of implementation/Repository/IDetialsRepository.cs
@@ -0,0 +1,9 @@
+﻿using bacics_of_implementation.Models;
+
+namespace bacics_of_implementation.Repository
+{
+    public interface IDetialsRepository
+    {
+        IEnumerable<details> GetAll();
+    }
+}
