0fd6bd459044c71d8e6bf7243f46a61b82e41187
Fayis KV <mohammedfayis135@gmail.com>
2026-02-25 11:51:33 +0530

    basics of DI

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        private 
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
