d40def05f1c8480bac7caeaafb07b35ed713e61e
Fayis KV <mohammedfayis135@gmail.com>
2026-03-04 16:20:08 +0530

    build an ednpoint with the list inside the controller

---
+++ bacics of implementation/bacics of implementation/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bacics_of_implementation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : Controller
+    {
+        class Person
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+        List<Person> person = new List<Person>
+        {
+            new Person {id = 1, name = "mhd"},
+            new Person {id = 2, name = "fayis"}
+        };
+        [HttpGet]
+        public IActionResult report()
+        {
+            return Ok(person);
+        }
+    }
+}
