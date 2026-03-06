ca548753fc0465a6a6abb00cca4e525376016455
Fayis KV <mohammedfayis135@gmail.com>
2026-03-06 15:30:37 +0530

    done the put and patch endpoints

--- bacics of implementation/bacics of implementation/Controllers/BaseController.cs
+++ bacics of implementation/bacics of implementation/Controllers/BaseController.cs
@@ -14,8 +14,6 @@ namespace bacics_of_implementation.Controllers
         }
         static List<Person> person = new List<Person>
                {
-               new Person {id = 1, name = "mhd"},
-               new Person {id = 2, name = "fayis"}
                };
         [HttpGet]
         public IActionResult report()
@@ -27,6 +25,23 @@ namespace bacics_of_implementation.Controllers
         public IActionResult addreport(Person per)
         {
             person.Add(per);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult editreport(int id, Person edited)
+        {
+            var editperson = person.FirstOrDefault(p => p.id == id);
+            editperson.name = edited.name;
+            editperson.id = edited.id;
+            return Ok();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult updatereport(int id, Person updated)
+        {
+            var updateperson = person.FirstOrDefault(p => p.id == id);
+            updateperson.name = updated.name;
             return Ok();
         }
 
