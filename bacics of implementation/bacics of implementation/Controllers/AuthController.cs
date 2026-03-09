bf50bf5ddbde066d90fa38dbe22888d33036a004
Fayis KV <mohammedfayis135@gmail.com>
2026-03-09 13:24:06 +0530

    basic auth endpoints

---
+++ bacics of implementation/bacics of implementation/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using bacics_of_implementation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bacics_of_implementation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        static List<Auth> user = new List<Auth>()
+        {
+
+        };
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] Auth data)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            user.Add(data);
+            return Ok("login successful");
+        }
+
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            return Ok(user);
+        }
+    }
+}
---
+++ bacics of implementation/bacics of implementation/Models/Auth.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bacics_of_implementation.Models
+{
+    public class Auth
+    {
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string Confirmpassword { get; set; }
+    }
+}
